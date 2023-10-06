9bb8c69c3cd489b6c71e0d083d32ee944471fcb8
ubaid ur rehman <110807502+helloword1100@users.noreply.github.com>
2023-10-07 01:29:11 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,26 @@
+const dat = document.getElementById('date');
+const day = document.getElementById('day');
+const month = document.getElementById('month');
+const year = document.getElementById('year');
+
+const today = new Date();
+const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+dat.innerHTML = (today.getDate()<10?"0":"")+today.getDate();
+day.innerHTML = weekDays[today.getDay()-1];
+month.innerHTML = months[today.getMonth()];
+year.innerHTML = today.getFullYear();
